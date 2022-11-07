b4284359082f75697e2b3aa9ee1bcbec51f62837
Claudio Melotti <claudio.melotti@eng.it>
2022-11-07 16:06:20 +0100

    primo commit branch main

--- src/app/member.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MemberDirective } from './member.directive';
-
-describe('MemberDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MemberDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/environments/environment.test.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiEquilibristiWs: 'https://equilibristi-app-ws-test.herokuapp.com'
-};
